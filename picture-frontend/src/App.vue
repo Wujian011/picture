2a4f9f5e3130f1254de36bb9daaf81e8ec39e44f
2457507566@qq.com <2457507566@qq.com>
2025-02-08 22:55:05 +0800

    项目初始化

---
+++ picture-frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <a-config-provider :locale="zhCN">
+      <BasicLayout />
+    </a-config-provider>
+  </div>
+</template>
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+import { useLoginUserStore } from '@/stores/userLoginUserStore'
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+</script>
+<style scoped>
+#app {
+}
+</style>
---
+++ picture-frontend/src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = '吴健的VUE前端项目模板'
+</script>
+
+<style scoped>
+#homePage {
+}
+</style>
